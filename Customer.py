fbad8b5eaaf7b8588daf48c271085c00788c0528
Yash Wadhvani <84962595+YashWadhvani@users.noreply.github.com>
2022-02-01 20:27:29 +0530

    Update Customer.py

--- Customer.py
+++ Customer.py
@@ -4,6 +4,7 @@ from tabulate import tabulate
 
 mydb = database_connector()
 mycursor = mydb.cursor()
+mycursor.execute("USE STORE_MANAGEMENT")
 
 
 class Customer:
@@ -18,12 +19,12 @@ class Customer:
         mycursor.execute("SELECT ID, NAME FROM CUSTOMERS")
         myresult = mycursor.fetchall()
         if myresult == []:
-            x = [0, None]
-        customers = []
+            myresult.append([0, None])
+        customers = {}
         for x in myresult:
-            customers.append(x[1])
+            customers[x[1]] = x[0]
         if Customer.name not in customers:
-            Owner.addCustomer(Owner.name, Customer.name)
+            Owner.addCustomer(Owner.name, Customer.name, customers[x[1]])
 
     @staticmethod
     def search_item(name):
@@ -59,12 +60,16 @@ class Customer:
                             print(
                                 f"{result[0][0]} {result[0][1]} of {result[0][2]} Left")
                             qty = int(input("Enter the Quantity of Item:-"))
-                            query = f"UPDATE STOCK SET STOCK_LEFT = {result[0][0] - qty} WHERE SRNO = {sr}"
-                            mycursor.execute(query)
-                            Customer.amount += (qty * result[0][3])
-                            items = [result[0][2], result[0]
+                            if qty > result[0][0]:
+                                print("Sorry! We don't have enough stock!\nPlease Check again in a few days!")
+                            else:
+                                query = f"UPDATE STOCK SET STOCK_LEFT = {result[0][0] - qty} WHERE SRNO = {sr}"
+                                mycursor.execute(query)
+                                Customer.amount += (qty * result[0][3])
+                                items = [result[0][2], result[0]
                                      [3], qty, (qty * result[0][3])]
-                            Customer.itemsPur.append(items)
+                                if items[2] > 0:
+                                    Customer.itemsPur.append(items)
                     ans = input("Do you want to buy more Items? (Y/N):-")
                     if ans not in "Yy":
                         break
@@ -77,25 +82,32 @@ class Customer:
                 f"SELECT ACCOUNT FROM CUSTOMERS WHERE name = '{Customer.name}';")
             myresult = mycursor.fetchall()
             for x in myresult:
-                amt = x[0]
+                if not x:
+                    amt = 0
+                else:
+                    amt = x[0]
             mycursor.execute(
                 f"UPDATE CUSTOMERS SET ACCOUNT = {amt} + {Customer.amount} where name = '{Customer.name}';")
             mydb.commit()
-            Customer.printInvoice()
+            Customer.printInvoice(Customer.name)
 
-        except:
-            print("Sorry! We don't have enough stock!\nPlease Check again in a few days!")
+        except Exception as e:
+            print(e)
+            
 
     def printInvoice(self):
         """Function to print the Sales Ivoice."""
 
-        print("="*14, "INVOICE", "="*14)
-        print("="*37)
-        print(tabulate(Customer.itemsPur, headers=[
-              "Item", "cost", "Quantity", "Amount"], tablefmt="fancy_grid"))
-        print("="*37)
-        print()
-        print(f"Dear {Customer.name}, Your Bill Amount = Rs. {Customer.amount}")
+        if Customer.itemsPur:
+            print("="*14, "INVOICE", "="*14)
+            print("="*37)
+            print(tabulate(Customer.itemsPur, headers=[
+                  "Item", "Cost", "Quantity", "Amount"], tablefmt="fancy_grid"))
+            print("="*37)
+            print()
+            print(f"Dear {Customer.name}, Your Bill Amount = Rs. {Customer.amount}")
+        else:
+            print("You haven't purchased anything!")
 
     def payBill(self):
         """Function to settle accounts by paying the outstanding bill."""
@@ -113,13 +125,15 @@ class Customer:
         if ch == 1:
             paid = int(input("Enter Amount :-"))
             amt -= paid
+            mycursor.execute(
+            f"UPDATE OWNERS SET ACCOUNT = ACCOUNT + {paid} WHERE NAME = '{Owner.name}'")
         elif ch == 2:
             amt = 0
+            mycursor.execute(
+            f"UPDATE OWNERS SET ACCOUNT = ACCOUNT + {amt} WHERE NAME = '{Owner.name}'")
         else:
             amt += (0.1*amt)
 
         mycursor.execute(
             f"UPDATE CUSTOMERS SET ACCOUNT = {amt} WHERE NAME = '{Customer.name}';")
-        mycursor.execute(
-            f"UPDATE OWNERS SET ACCOUNT = ACCOUNT + {amt} WHERE NAME = {Owner.name}")
         mydb.commit()
